def08951639a07bfa74962bfa8eac3d9e8baeb09
needf <needforspeedun3@gmail.com>
2023-06-17 21:04:35 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_note_app/views/noties_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(useMaterial3: true),
+      // theme: ThemeData(
+      //   brightness: Brightness.dark,
+      //   scaffoldBackgroundColor: Colors.white30
+      // ),
 
 
       home: const NotesApp(),
@@ -19,9 +25,8 @@ class MyApp extends StatelessWidget {
 }
 class NotesApp extends StatelessWidget {
   const NotesApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(),);
+    return Scaffold(body: const NotesView(),);
   }
 }
